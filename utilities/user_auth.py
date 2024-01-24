389ffc71b9f212cb1a3b902640db1fe63b983de1
alirahmanicode <alirahmanicode@gmail.com>
2024-01-24 11:12:17 +0330

    get current user

--- utilities/user_auth.py
+++ utilities/user_auth.py
@@ -1,7 +1,15 @@
 from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
+from fastapi import Depends, status
+from fastapi.exceptions import HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from jose import JWTError, jwt
+from sqlalchemy.orm import Session
 
+from dbmanagement import crud
+from dependencies.dependencies import get_db
 
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
@@ -9,6 +17,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 def get_password_hash(password):
@@ -32,3 +41,27 @@ def authenticate_user(user_in_db, password) -> bool:
 
 def verify_password(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise
+    except JWTError:
+        raise
+
+    user = crud.get_user_by_username(db=db, username=username)
+
+    if user is None:
+        raise credentials_exception
+    return user
