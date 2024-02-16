a78c32676feee8b691813be417fd2bf3d4cb91a3
alirahmanicode <alirahmanicode@gmail.com>
2024-02-16 11:04:03 +0330

    update

--- routers/chat.py
+++ routers/chat.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-import json
 from typing import Annotated, List
 
 from fastapi import (
@@ -67,7 +66,7 @@ async def privet_message(request: Request, target_user_id: int, db=Depends(get_d
     )
 
 
-@router.websocket("/ws/{current_user_id}/{target_user_id}")
+@router.websocket("/{current_user_id}/{target_user_id}")
 async def privet_message_websocket(
     websocket: WebSocket, current_user_id: int, target_user_id: int, db=Depends(get_db)
 ):
@@ -85,7 +84,7 @@ async def privet_message_websocket(
             chat_obj = crud.create_chat(
                 db=db, users_id=[current_user_id, target_user_id]
             )
-            
+
             message = schemas.Message(
                 text=data,
                 is_seen=False,
@@ -101,28 +100,18 @@ async def privet_message_websocket(
 
     except WebSocketDisconnect:
         manager.disconnect(user.id)
-        await manager.broadcast(f"Client #{user.username} left the chat")
 
 
-# @router.websocket("/ws/{current_user_id}/{target_user_id}")
-# async def privet_message_websocket(
-#     websocket: WebSocket, current_user_id: int, target_user_id: int, db=Depends(get_db)
-# ):
-#     """
-#     The client send message and the target user recieve the message.
-#     """
-#     user = crud.get_user_by_id(db=db, user_id=current_user_id)
-
-#     await manager.connect(websocket=websocket, user_id=current_user_id)
-
-#     try:
-#         while True:
-#             data = await websocket.receive_text()
-#             print(json.loads(data)["message"])
-#             # is_target_user_is_online = manager.is_user_online(user_id=target_user_id)
-#             # if is_target_user_is_online:
-#             #     await manager.send_personal_message(data, target_user_id)
-
-#     except WebSocketDisconnect:
-#         manager.disconnect(user.id)
-#         await manager.broadcast(f"Client #{user.username} left the chat")
+@router.websocket("/check_connection/{current_user_id}/{target_user_id}")
+async def is_user_online(
+    websocket: WebSocket, current_user_id: int, target_user_id: int
+):
+    await manager.set_connection_socket(websocket=websocket, user_id=current_user_id)
+    is_online = manager.is_user_online(target_user_id)
+    await websocket.send_text(f"{is_online}")
+    try:
+        while True:
+            data = await websocket.receive_text()
+    except WebSocketDisconnect:
+        if is_online:
+            await manager.send_connection_info(f"False", target_user_id)
--- routers/connection_manager.py
+++ routers/connection_manager.py
@@ -7,7 +7,13 @@ class ConnectionManager:
 
     async def connect(self, websocket: WebSocket, user_id: int) -> None:
         await websocket.accept()
-        self.active_connections[user_id] = websocket
+        sockets_obj = self.get_user_socket_object(websocket)
+        self.active_connections[user_id] = sockets_obj
+
+    async def set_connection_socket(self, websocket: WebSocket, user_id: int) -> None:
+        await websocket.accept()
+        self.active_connections.get(user_id)["connection_socket"] = websocket
+        print(self.active_connections)
 
     def is_user_online(self, user_id: int) -> bool:
         return self.active_connections.get(user_id, False) and True
@@ -16,10 +22,16 @@ class ConnectionManager:
         del self.active_connections[user_id]
 
     async def send_personal_message(self, message: str, target_user_id: int):
-        websocket = self.active_connections.get(target_user_id)
-        # await websocket.send_text(message)
+        websocket: WebSocket = self.active_connections.get(target_user_id).get(
+            "message_socket"
+        )
         await websocket.send_text(message)
 
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text()
+    async def send_connection_info(self, message: str, target_user_id: int) -> None:
+        websocket: WebSocket = self.active_connections.get(target_user_id).get(
+            "connection_socket"
+        )
+        await websocket.send_text(message)
+
+    def get_user_socket_object(self, user_websocket: WebSocket):
+        return {"message_socket": user_websocket, "connection_socket": None}
